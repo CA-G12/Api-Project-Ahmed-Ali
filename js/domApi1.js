a5adeaa1357123cbe7d8818bdb9dc1603ec4dee5
Ahmed Abu Sharar <absam9945@gmail.com>
2022-08-03 16:09:40 +0300

    some edits and made main data displayed dynamically Relates #10

--- js/domApi1.js
+++ js/domApi1.js
@@ -3,34 +3,47 @@ const characters = 'https://breakingbadapi.com/api/characters';
 const quotes = 'https://breakingbadapi.com/api/quotes';
 const input = document.querySelector("input")
 const buttonSearch = document.querySelector("button")
-let div1 = document.createElement('div')
-let img = document.createElement('img')
-let div2 = document.createElement('div')
-let div3 = document.createElement('div')
-let h3 = document.createElement('h3')
-let h5_1 = document.createElement('h5')
-let h5_2 = document.createElement('h5')
+let loader = document.querySelector(".loader");
 let h5_3 = document.createElement('h5')
 let allQuotes = document.querySelector(".all-quotes")
 let charactersContainer = document.querySelector(".characters-container");
 
+
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
+})
+
 buttonSearch.addEventListener('click' , ()=>{
     if(input.value){
+        charactersContainer.innerHTML= "";
         fetch(characters, (data)=>{
             characterDetails(allFunctions.search(data,input.value , "name")[0]); 
             fetch(quotes, (data)=>{
                 someQuotes(allFunctions.search(data,input.value , "author") ); 
             })
         })
-
-        
-
     }
-   
 }
 );
-function characterDetails (character){
-    charactersContainer.innerHTML =``;
+for(let i = 0 ; i < 6 ; i++){
+    fetch(characters, (data)=>{
+        characterDetails(data[i]); 
+    })
+}
+
+
+
+function characterDetails (character ,){
+    let div1 = document.createElement('div')
+let img = document.createElement('img')
+let div2 = document.createElement('div')
+let div3 = document.createElement('div')
+let h3 = document.createElement('h3')
+let h5_1 = document.createElement('h5')
+let h5_2 = document.createElement('h5')
+
+
+
     div1.classList.add('character');
     img.classList.add('character-img');
     div2.classList.add('overlay');
@@ -55,6 +68,7 @@ function characterDetails (character){
     div1.appendChild(div2)
     charactersContainer.appendChild(div1)
 }
+
 function someQuotes(quotes){
     h5_3.innerText = quotes[0].quote;
     div3.appendChild(h5_3)
