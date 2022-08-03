0faee5824643285d1490ccf61c4720a10fdb6304
ShbairAli <ali89freelancer@gmail.com>
2022-08-03 15:56:54 +0300

    fetched gifs are ready to be presented in its section, on load and when searching about certain character Relates #6

--- js/domApi2.js
+++ js/domApi2.js
@@ -1,2 +1,16 @@
+let gifsContainer = document.querySelector('.all-gif');
+fetch(allFunctions.replaceUrl('Walter White'), (data) => renderGifs(data));
+buttonSearch.addEventListener('click', () => {
+  fetch(allFunctions.replaceUrl(input.value), (data) => renderGifs(data));
+});
 
-
+const renderGifs = (data) => {
+  for (let i = 0; i < 10; i++) {
+    gifsContainer.innerHTML += `
+            <div class="gif">
+                <img src="${data.data[i].images.downsized_medium.url}"
+                    alt="">
+            </div>
+  `;
+  }
+};
